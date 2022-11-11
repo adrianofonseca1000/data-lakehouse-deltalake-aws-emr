9c1beed5a91a241cd4323f036e07f6c72d85d75d
Adriano Fonseca <30940643+adrianofonseca1000@users.noreply.github.com>
2022-11-11 10:04:28 -0300

    Sobe Infra Airflow

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,10 +10,10 @@ variable "key_pair_name" {
   default = "adriano-teste"
 }
 
-# variable "airflow_subnet_id" {
-#   default = "subnet-4cef5427"
-# }
+variable "airflow_subnet_id" {
+  default = "subnet-09f7790fe695d10fe"
+}
 
-# variable "vpc_id" {
-#   default = "vpc-d724b4bc"
-# }
+variable "vpc_id" {
+  default = "vpc-08179adaab017fe5c"
+}
